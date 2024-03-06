a4d76386378246b2fb59cf420e5180426f5ecaae
Hussain Khorakiwala <hussainkhorakiwala45@outlook.com>
2024-03-06 22:24:09 +0530

    Minor Changes

--- Linux/Install-Virt-Manager.sh
+++ Linux/Install-Virt-Manager.sh
@@ -1,5 +1,10 @@
-paru -S qemu virt-manager --noconfirm
-paru -S ebtables --noconfirm
-sudo systemctl enable libvirtd
-sudo systemctl start libvirtd
-sudo usermod -a $USER -G libvirt
+sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq bridge-utils libguestfs ebtables vde2 openbsd-netcat --noconfirm
+
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g; s/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
+
+sudo usermod -aG libvirt $USER
+
+sudo systemctl restart libvirtd.service
