852a2349a132fb64d661534ee751b9c2bf319b74
Hussain Khorakiwala <hussainkhorakiwala45@outlook.com>
2024-03-06 19:09:57 +0530

    A few changes

--- Linux/Import-All-Configs.sh
+++ Linux/Import-All-Configs.sh
@@ -1,5 +1,5 @@
 ########### Taking backups of Files if they exist ###########
-mv -rf .config/alacritty .config/qtile .config/starship.toml .zshrc backups-$(date +%Y.%m.%d-%H.%M.%S)
+mv -rf ~/.config/alacritty ~/.config/qtile ~/.config/starship.toml ~/.zshrc ~/backups-$(date +%Y.%m.%d-%H.%M.%S)
 
 ########### CLoning the directory ###########
 mkdir -p ~/tempDotfiles
@@ -19,11 +19,15 @@ mv -rf ~/tempDotfiles/Linux/.zshrc .
 rm -rf ~/tempDotfiles
 
 ########### Installing Packages ###########
-sudo pacman -S zsh, qtile, qtile-extras, starship, alacritty, rofi, brave, thunar, ttf-ubuntu-font-family, ttf-ubuntu-nerd, ttf-ubuntu-mono-nerd, ttf-jetbrains-mono, ttf-jetbrains-mono-nerd --noconfirm
+sudo pacman -S zsh, qtile, qtile-extras, starship, alacritty, rofi, brave, thunar, ttf-ubuntu-font-family, ttf-ubuntu-nerd, ttf-ubuntu-mono-nerd, ttf-jetbrains-mono, ttf-jetbrains-mono-nerd, picom --noconfirm
 yay -S shell-color-scripts --noconfirm
 
 ########### Setting up Configs ###########
+mkdir -p ~/.zsh-plugins
 cd ~/.zsh-plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search
+
+########### Changing Defaults ###########
+chsh -s /bin/zsh $USER
