29a92dc2b76b8619657431ba6dbb6686d2d54e09
AverageBlank <hussainkhorakiwala45@gmail.com>
2024-08-22 15:32:57 +0530

    Changes to NeoVim - Made it so that C-f opens tmux in every mode.

--- .config/nvim/lua/config/remaps.lua
+++ .config/nvim/lua/config/remaps.lua
@@ -42,7 +42,7 @@ vim.keymap.set('n', '<CR>', 'o<Esc>', { desc = 'Create new line without exiting 
 vim.keymap.set('i', '<C-C>', '<Esc>')
 
 -- Switching Projects
-vim.keymap.set('n', '<C-f>', '<cmd> silent !tmux neww ~/.config/tmuxthing<CR>')
+vim.keymap.set({ 'i', 'v', 'n' }, '<C-f>', '<cmd> silent !tmux neww ~/.config/tmuxthing<CR>')
 
 -- Find and Replace
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Find and Replace' })
